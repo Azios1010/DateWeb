592475c1e04548c1713de9e40a70a0acee5fb3c0
Azios <hyp13579@gmail.com>
2025-12-16 21:53:25 +0700

    change API server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ import CoupleHeader from './components/CoupleHeader';
 // Link API (nhớ đổi thành link Render của bạn nếu deploy)
 //const API_URL = 'https://date-require-website.onrender.com/api/couple';
 
-const API_URL = 'http://localhost:3000/api/couple';
+const API_URL = 'https://date-require-website.onrender.com';
 
 function App() {
   const [coupleId, setCoupleId] = useState(null);
